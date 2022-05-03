d046304d4ad7729fb61034bde4edf68a2d1febe2
braddefauw <badefauw@gmail.com>
2022-05-03 10:35:49 -0500

    added home button event listener

--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@ import { menu } from './menu'
 import { contact } from './contact'
 
 pageLoad();
+let content = document.querySelector("#content");
+
 let menuBtn = document.querySelector(".menu");
 menuBtn.addEventListener('click', function(){
-    let content = document.querySelector("#content");
     content.remove();
     menu();
 })
 let contactBtn = document.querySelector(".contact");
 contactBtn.addEventListener('click', function() {
+    content.remove();
     contact();
+})
+let homeBtn = document.querySelector(".home");
+homeBtn.addEventListener('click', function() {
+    content.remove();
+    pageLoad();
 })
 // menu();
 // contact();
